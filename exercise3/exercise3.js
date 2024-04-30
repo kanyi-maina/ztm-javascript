5a04fb4c5e7f73540bc339cc5d7dd9eb61651824
nickanyi <130154999+kanyi-dev@users.noreply.github.com>
2024-04-30 17:47:26 +0300

    code for exercise3

---
+++ exercise3/exercise3.js
@@ -0,0 +1,7 @@
+//Can only run in a browswer as node JS has no window element to Prompt
+
+var first = prompt("Please enter the First number: ");
+var second = prompt("Please enter the Second number: ");
+
+var sum = Number(first) + Number(second);
+alert(sum);
