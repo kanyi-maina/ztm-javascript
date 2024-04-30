5641871b0775ecb3cb921df9d325f1628d48bcf2
nickanyi <130154999+kanyi-dev@users.noreply.github.com>
2024-04-30 19:09:12 +0300

    call directly from console

--- javascript_webpage/script.js
+++ javascript_webpage/script.js
@@ -14,4 +14,5 @@ const multiply = (a, b) => {
   return a * b;
 };
 
-console.log(multiply(5, 10));
+// console.log(multiply(5, 10));
+//Since I'veremoved the console.log, you need to call it in the console itself in order to work
