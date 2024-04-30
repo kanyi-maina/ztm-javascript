6567f29e8b46fbdf7ae4d5ed9e6a9261d6ba303a
nickanyi <130154999+kanyi-dev@users.noreply.github.com>
2024-04-30 18:31:41 +0300

    working code

---
+++ exercise4/exercise4.js
@@ -0,0 +1,8 @@
+var age = prompt("How old are you?: ");
+if (parseInt(age) === 18) {
+  console.log("Congratulations on your first year of driving. Enjoy the Ride");
+} else if (parseInt(age) < 18) {
+  console.log("Sorry, you are too young to drive this car. Powering off");
+} else if (parseInt(age) > 18) {
+  console.log("Powering on. Enjoy the ride!");
+}
