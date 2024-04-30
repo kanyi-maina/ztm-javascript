b8471114bcc89ed39035d44795abd432ff9f455d
nickanyi <130154999+kanyi-dev@users.noreply.github.com>
2024-04-30 20:04:08 +0300

    all through to lesson 6

--- javascript_webpage/script.js
+++ javascript_webpage/script.js
@@ -1,18 +1,2 @@
-const sayHello = () => {
-  console.log("Hello");
-};
-
-sayHello();
-
-const sayBye = () => {
-  console.log("Bye!");
-};
-
-sayBye();
-
-const multiply = (a, b) => {
-  return a * b;
-};
-
-// console.log(multiply(5, 10));
-//Since I'veremoved the console.log, you need to call it in the console itself in order to work
+let list = [["tiger", "cat", "bear", "bird"]];
+console.log(list[0][2]);
