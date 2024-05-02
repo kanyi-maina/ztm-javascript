889f465fe82905c99e56e0ff8a826f1289bce2c7
nickanyi <130154999+kanyi-dev@users.noreply.github.com>
2024-05-02 13:28:12 +0300

    starter code

---
+++ facebook/script.js
@@ -0,0 +1,20 @@
+var database = [
+  {
+    username: "Nicholas",
+    password: "mysecretword",
+  },
+];
+
+var newsFeed = [
+  {
+    username: "Bobby",
+    timeline: "So tired from all this amazing learning",
+  },
+  {
+    username: "Sally",
+    timeline: "JavaScript is SOOOO cool!",
+  },
+];
+
+var userNamePrompt = prompt("What is your name?");
+var passwordPrompt = prompt("What is your password?");
