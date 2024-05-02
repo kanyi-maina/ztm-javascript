2b553fe7ca4087b8c0558580fb54aa7cad576ef6
nickanyi <130154999+kanyi-dev@users.noreply.github.com>
2024-05-02 12:44:11 +0300

    tasks completed

---
+++ exercise7/exercise7.js
@@ -0,0 +1,21 @@
+const obj = {
+  username: "nickanyi",
+  password: "abc12345",
+};
+
+const database = [obj];
+
+const newsfeed = [
+  {
+    username: "notothious",
+    timeline: "4 years",
+  },
+  {
+    username: "notothious",
+    timeline: "5 years",
+  },
+  {
+    username: "notothious",
+    timeline: "6 years",
+  },
+];
