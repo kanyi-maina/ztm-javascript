af9cd15874e018b5aa1bcc0b9df93ea19a2bf1f4
nickanyi <130154999+kanyi-dev@users.noreply.github.com>
2024-05-02 14:40:31 +0300

    terminologies

---
+++ Notes/terminology.js
@@ -0,0 +1,29 @@
+//Function declaration
+function newFunction() {}
+
+//Function expression
+const anotherFunc = function () {};
+
+//expression
+1 + 3;
+let a = 2;
+return true;
+
+//calling or invoking a function
+alert();
+newFunction(param1, param2);
+
+//assign a variable
+let b = true;
+
+//function vs method
+const thisIsAFunction = () => {};
+
+const obj = {
+    thisIsAMethod: () => {}
+}
+
+//Call a Function
+thisIsAFunction();
+//Call a method
+obj.thisIsAMethod();
