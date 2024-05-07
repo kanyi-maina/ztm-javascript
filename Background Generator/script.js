223737d5ddab5e7eaa9ac2fc02720edf80bd6590
nickanyi <130154999+kanyi-dev@users.noreply.github.com>
2024-05-08 02:06:05 +0300

    prettier format

--- Background Generator/script.js
+++ Background Generator/script.js
@@ -3,13 +3,12 @@ let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
 let body = document.getElementById("gradient");
 
-console.log(body)
+console.log(body);
 
 color1.addEventListener("input", () => {
   console.log(color1.value);
 });
 
 color2.addEventListener("input", () => {
-    console.log(color2.value);
-  });
-
+  console.log(color2.value);
+});
