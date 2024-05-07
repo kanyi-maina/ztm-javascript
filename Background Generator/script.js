d8baf9b5381ec36c5284b079bb2223b303e27a97
nickanyi <130154999+kanyi-dev@users.noreply.github.com>
2024-05-08 01:58:19 +0300

    add log when color input is clicked

--- Background Generator/script.js
+++ Background Generator/script.js
@@ -1,1 +1,7 @@
 let css = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+
+color1.addEventListener("input", () => {
+  console.log(color1.value);
+});
