dfa64f8043fdc274feaabea7e8bb7c72e9649263
nickanyi <130154999+kanyi-dev@users.noreply.github.com>
2024-05-10 12:10:00 +0300

    create a function to call on both methods

--- Background Generator/script.js
+++ Background Generator/script.js
@@ -3,13 +3,11 @@ let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
 let body = document.getElementById("gradient");
 
-console.log(body);
-
-color1.addEventListener("input", () => {
+const setGradient = () => {
   body.style.background =
-    "linear-gradient(to right, " + color1.value + ", " + color2.value + ");";
-});
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+};
 
-color2.addEventListener("input", () => {
-  console.log(color2.value);
-});
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
