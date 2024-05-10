059ac5c7756a93d66160c3483da4e46c9032641e
nickanyi <130154999+kanyi-dev@users.noreply.github.com>
2024-05-10 12:03:16 +0300

    create the fade style on the event listener

--- Background Generator/script.js
+++ Background Generator/script.js
@@ -6,7 +6,8 @@ let body = document.getElementById("gradient");
 console.log(body);
 
 color1.addEventListener("input", () => {
-  console.log(color1.value);
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ");";
 });
 
 color2.addEventListener("input", () => {
