f42ab56567f639a4809509adcddcde4d19afa5ad
nickanyi <130154999+kanyi-dev@users.noreply.github.com>
2024-05-15 10:09:45 +0300

    added code

---
+++ Random/switch.js
@@ -0,0 +1,20 @@
+function moveCommand(direction) {
+  let whatHappens;
+  switch (direction) {
+    case "forward":
+      whatHappens = "You encounter a monster";
+      break;
+    case "forward":
+      whatHappens = "You encounter a monster";
+      break;
+    case "forward":
+      whatHappens = "You encounter a monster";
+      break;
+    case "forward":
+      whatHappens = "You encounter a monster";
+      break;
+    default:
+      whatHappens = "Please enter a valid direction";
+  }
+  return whatHappens;
+}
